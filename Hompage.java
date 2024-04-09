7ab39190de5b52ad3a57c03d47a3e6dacc2f3e5e
Saren Sokmeak <120242475+Sokmeak@users.noreply.github.com>
2024-04-09 16:16:24 +0700

    Delete Hompage.java

--- Hompage.java
+++
@@ -1,7 +0,0 @@
-class Homepage{
-    public static void main(String[] args) {
-        System.out.println("Hello Java");
-        System.out.println("This is my Homepage");
-        System.out.println("No, it's mine");
-    }
-}
