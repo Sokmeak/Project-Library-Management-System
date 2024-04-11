c3f8f9c0bdba167568de8d1641af5b818698183d
Sokmeak <saroussokmeak721@gmail.com>
2024-04-11 10:00:32 +0700

    update about java for interface file!

---
+++ LibraryManagementSystemGroup04/src/main/java/com/mycompany/itc/librarymanagementsystemgroup04/Homepage.java
@@ -0,0 +1,9 @@
+package com.mycompany.itc.librarymanagementsystemgroup04;
+class Homepage{
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("This is my Homepage");
+        System.out.println("Here to go now!");
+        
+    }
+}
