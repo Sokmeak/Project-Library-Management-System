3c5b972c717b108ac453a2239ba57c1619978ca5
Sokmeak <saroussokmeak721@gmail.com>
2024-04-18 10:04:21 +0700

    insert 1 line for Test1 file and add a new file named User_security

--- LibraryManagementSystem_Group05/src/librarymanagementsystem_group05/Test1.java
+++ LibraryManagementSystem_Group05/src/librarymanagementsystem_group05/Test1.java
@@ -6,6 +6,8 @@ public class Test1 {
     
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Everyone!");
+                
     }
     
 }
---
+++ LibraryManagementSystem_Group05/src/librarymanagementsystem_group05/User_Security.java
@@ -0,0 +1,11 @@
+
+package librarymanagementsystem_group05;
+
+
+public class User_Security {
+    public static void main(String[] args) {
+        System.out.println("This class for user's security!");
+    }
+            
+    
+}
