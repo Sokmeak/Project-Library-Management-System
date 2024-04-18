fa1c4d7688eeebde324c2c45bda6ecb0244a092a
Sokmeak <saroussokmeak721@gmail.com>
2024-04-18 15:39:35 +0700

    New file project to Repository with icons files

---
+++ LibraryManagementSystem_Group05/src/Test/Test1.java
@@ -0,0 +1,13 @@
+
+package Test;
+
+
+public class Test1 {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Everyone!");
+                
+    }
+    
+}
---
+++ LibraryManagementSystem_Group05/src/Test/User_Security.java
@@ -0,0 +1,12 @@
+
+package Test;
+
+
+public class User_Security {
+    public static void main(String[] args) {
+        System.out.println("This class for user's security!");
+        System.out.println("Welcome to security file");
+    }
+            
+    
+}
