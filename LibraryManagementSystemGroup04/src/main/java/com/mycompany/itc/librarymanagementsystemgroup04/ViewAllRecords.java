87e362b5183f3d470648ff46012abcd210acbfe2
SINA SINAT <163482073+SSINAT@users.noreply.github.com>
2024-04-22 08:16:03 +0700

    test

---
+++ LibraryManagementSystemGroup04/src/main/java/com/mycompany/itc/librarymanagementsystemgroup04/ViewAllRecords.java
@@ -0,0 +1,10 @@
+package com.mycompany.itc.librarymanagementsystemgroup04;
+public class viewrecords{
+    
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Everyone!");
+                
+    }
+    
+}
